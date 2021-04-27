ecfb093bda459cdc4d25c92f16b3a2658f15e85d
Aiden <k1ard@yahoo.com>
2021-04-26 20:07:17 -0400

    Remove token from code into .env

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
-import logging
+import logging, sys
+
+from dotenv import dotenv_values
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -8,10 +10,13 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
+# Init Logging
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+
+# Initialize Client
 client = MyClient()
-client.run("ODM2MzIxMjcwNjQzNjg3NTI1.YIcS4Q.dbAbczn2pTVGLKlubtR5EsXS3EI")
+client.run(dotenv_values(".env")["TOKEN"])
