7285526361803e794c13bd0996c55893b1923fb5
Aiden <k1ard@yahoo.com>
2021-04-27 11:44:54 -0400

    move logs to dedicated folder; make daily logs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-import logging, sys
+import logging, sys, datetime
 
 from dotenv import dotenv_values
 
@@ -17,7 +17,7 @@ class MyClient(discord.Client):
 # Init Logging
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='logs/discord'+str(datetime.date.today())+'.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
