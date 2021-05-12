7bc7809e19723806a521b8b0e1613bc2c372377e
Aiden <k1ard@yahoo.com>
2021-05-12 11:24:56 -0400

    add status message showing prefix

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 from world import World
 
@@ -12,9 +13,11 @@ async def pingpong(ctx):
 @bot.command(name="prefix")
 async def prefix(ctx, new_prefix):
     bot.command_prefix = new_prefix
-    print("Set prefix to " + new_prefix)
+    print("Set prefix to " + bot.command_prefix)
+    await bot.change_presence(activity=discord.Game(name="Prefix is "+bot.command_prefix))
 
 @bot.event
 async def on_ready():
     for guild in bot.guilds:
         print("Connected to " + guild.name)
+    await bot.change_presence(activity=discord.Game(name="Prefix is "+bot.command_prefix))
